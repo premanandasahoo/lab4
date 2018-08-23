87d71be8f37dbb4d8267783f86e0783651798956
premanandasahoo <42239894+premanandasahoo@users.noreply.github.com>
2018-08-23 19:02:56 +0530

    Add files via upload

---
+++ lab4_q4.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+	int main(){
+float celsius,fahrenheit;
+cout<<"what is temperature in celsius";
+cin>>celsius;
+fahrenheit=32+(9*celsius)/5;
+cout<<"temperature is fahrenheit"<<fahrenheit;
+return 0;
+}
---
+++ lab4_q5.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+	int main(){
+int base,height,area;
+cout<<"enter your base and height";
+cin>>base>> height;
+area=(1.0/2)*base*height;
+cout<<"the area of triangle "<<area;
+return 0;
+}
